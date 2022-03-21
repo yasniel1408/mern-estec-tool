d2f8b63218faae351f876074b094890e7be39eb5
Yasniel Fajardo Egues <54322191+yasniel1408@users.noreply.github.com>
2022-03-20 22:04:52 -0400

    Update configSqls.js

--- db/configSqls.js
+++ db/configSqls.js
@@ -1,5 +1,5 @@
 const config = {
-    connectionSQL: "Data Source=kali\\SQL2012;Initial Catalog=UNE_2955A_INT;Persist Security Info=True;User ID=sa;Password=Super2009;"
+    connectionSQL: "Connection String"
 }
 
 module.exports = config;    
